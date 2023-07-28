ab45061cc845f2b4b27b6ea8b11a2730456c9fa0
FernandoAlexis <blancogonzalezfernandoalexis@gmail.com>
2023-07-28 08:32:14 -0500

    Cambios en conexion

--- conexiones/parametros.php
+++ conexiones/parametros.php
@@ -1,7 +1,7 @@
 <?php
 define("USUARIO","MCAMPOS");
 define("PASSWORD", "exmcampos");
-define("SOFTLAND", "eximp600");
+define("SOFTLAND", "SOFTLAND");
 define("DATABASE_BODEGA", "BODEGA");
 define("SERVIDOR","192.168.0.44");
 ?>
